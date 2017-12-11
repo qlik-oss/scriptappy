d40439f877d17a5fd903107475b14357cd77d682
Miralem Drek <mm85dk@gmail.com>
2017-12-11 11:05:41 +0100

    Add example

---
+++ packages/jsdoc2spec/examples/nodejs/src/net.js
@@ -0,0 +1,54 @@
+/**
+ * @module net
+ * @stability 2
+ */
+
+/**
+ * This class is used to create a TCP or ICP server
+ */
+export class Server {
+  /**
+   * Returns the bound address.
+   * @returns {module:net.Server~address}
+   */
+  address() {}
+
+  /**
+   * Stops the server from accepting new connections and keeps existing connections
+   */
+  close() {}
+}
+
+/**
+ * @typedef {object} module:net.Server~address
+ * @property {number} port
+ * @property {string} family
+ * @property {string} address
+ */
+
+/**
+ * Emitted when the server closes
+ * @event module:net.Server.close
+ */
+
+/**
+ * Emitted when a new connection is made.
+ * @event module:net.Server.connect
+ * @param {module:net.Socket} socket - The connection object
+ */
+
+/**
+ * Creates a new TCP or ICP server.
+ * @param {object} [options]
+ * @param {boolean} [options.allowHalfOpen=false] - Indicates whether half-opened TCP connections are allowed.
+ * @param {boolean} [options.pauseOnConnect=false] - Indicates whether the socket should be paused on incoming connections.
+ * @param {function} [connectionsListener] - Automatically sets a listener for the `connection` event.
+ * @returns {module:net.Server}
+ */
+export function createServer(options, connectionListener) {}
+
+
+/**
+ * A number, or a string containing a number.
+ * @typedef {(number|string)} NumberLike
+ */
---
+++ packages/jsdoc2spec/examples/nodejs/src/path.js
@@ -0,0 +1,24 @@
+/**
+ * @module path
+ * @stability 2
+ */
+
+/**
+ * Returns the directory name of a `path`
+ * @param {string} path
+ * @returns {string}
+ */
+export function dirname(path) {}
+
+/**
+ * Returns the extension of the `path`
+ * @param {string} path
+ * @returns {string}
+ */
+export function extname(path) {}
+
+/**
+ * Provides acces to Window-specific implementations of the `path` methods
+ * @type {object}
+ */
+export const win32 = {};
