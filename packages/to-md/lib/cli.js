eb1615a91316c016b5606db514207c1f8bba1ffe
Miralem Drek <mm85dk@gmail.com>
2019-10-27 20:22:43 +0100

    feat(to-md): add cli entrypoint

---
+++ packages/to-md/lib/cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+/* eslint no-unused-expressions: 0 */
+const yargs = require('yargs');
+const fs = require('fs');
+const path = require('path');
+
+const toMd = require('./index');
+
+const toMdCommand = {
+  command: 'to-md',
+  desc: 'Build supernova',
+  builder(y) {
+    y.option('spec', {
+      type: 'string',
+      alias: 's',
+      required: true,
+    });
+    y.option('output', {
+      type: 'string',
+      alias: 'o',
+      default: 'api.md',
+    });
+  },
+  handler(argv) {
+    const spec = require(path.resolve(process.cwd(), argv.spec)); // eslint-disable-line
+    const md = toMd(spec);
+    const output = `
+
+# ${spec.info.name}
+
+> version: ${spec.info.version}
+
+## Table of contents
+
+${md.toc()}
+
+## API
+
+${md.content()}
+
+${md.references()}
+`;
+    fs.writeFileSync(argv.output, output);
+  },
+};
+
+yargs
+  .usage('sy <command> [options]')
+  .command(toMdCommand)
+  .demandCommand()
+  .alias('h', 'help')
+  .wrap(Math.min(80, yargs.terminalWidth())).argv;
