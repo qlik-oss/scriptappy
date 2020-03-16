c8c025f1abef1c5cd0f95d497495e9be4f7b8e74
Miralem Drek <mm85dk@gmail.com>
2020-03-16 07:25:50 +0100

    feat(to-md): add watch option

--- packages/to-md/lib/cli.js
+++ packages/to-md/lib/cli.js
@@ -1,28 +1,14 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const chokidar = require('chokidar');
 
 const toMd = require('./index');
 
-const toMdCommand = {
-  command: 'to-md',
-  desc: 'Generate API reference in markdown',
-  builder(y) {
-    y.option('spec', {
-      type: 'string',
-      alias: 's',
-      required: true,
-    });
-    y.option('output', {
-      type: 'string',
-      alias: 'o',
-      default: 'api.md',
-    });
-  },
-  handler(argv) {
-    const spec = require(path.resolve(process.cwd(), argv.spec)); // eslint-disable-line
-    const md = toMd(spec);
-    const output = `
+function doIt(specFile, destination) {
+  const spec = require(specFile); // eslint-disable-line
+  const md = toMd(spec);
+  const output = `
 
 # ${spec.info.name}
 
@@ -38,7 +24,41 @@ ${md.content()}
 
 ${md.references()}
 `;
-    fs.writeFileSync(argv.output, output);
+  fs.writeFileSync(destination, output);
+}
+
+const toMdCommand = {
+  command: 'to-md',
+  desc: 'Generate API reference in markdown',
+  builder(y) {
+    y.option('spec', {
+      type: 'string',
+      alias: 's',
+      required: true,
+    });
+    y.option('output', {
+      type: 'string',
+      alias: 'o',
+      default: 'api.md',
+    });
+    y.option('watch', {
+      type: 'boolean',
+      alias: 'w',
+      default: false,
+    });
+  },
+  handler(argv) {
+    const specFile = path.resolve(process.cwd(), argv.spec);
+    doIt(specFile, argv.output);
+    if (argv.w) {
+      console.log('\n\nWatching changes...');
+      chokidar.watch([specFile]).on('change', () => {
+        delete require.cache[specFile];
+        doIt(specFile, argv.output);
+
+        console.log('  ...updated');
+      });
+    }
   },
 };
 
