2bf3e382a175af78b3545da181135f42f786db3c
Thomas <thomas.bengtsson@qlik.com>
2021-12-20 14:51:24 +0100

    feat(to-dts): generate functions as type, support entries on fns

--- packages/to-dts/lib/types/function.js
+++ packages/to-dts/lib/types/function.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const dom = require('dts-dom');
 const params = require('./params');
 
@@ -10,10 +11,29 @@ module.exports = function fn(def, tsParent, g) {
   if (tsParent && ['union', 'array', 'parameter', 'alias'].includes(tsParent.kind)) {
     return dom.create.functionType(par, ret);
   }
+  if (def.entries) {
+    const t = dom.create.objectType([]);
+    const cs = dom.create.callSignature(par, ret);
+
+    // optional/rest flags for params in call-signatures are NOT printed by dts-dom,
+    // so hack it a bit by ammending ... or ? to the name of the param to get the same effect
+    // TODO - remove this hack when dts-dom fixes this
+    cs.parameters.forEach(p => {
+      if ((p.flags || 0) & dom.ParameterFlags.Optional) {
+        p.name += '?';
+      } else if (p.flags === dom.ParameterFlags.Rest) {
+        p.name = `...${p.name}`;
+      }
+      p.flags = 0; // reset to avoid flags being used if dts-dom supports it before above code has been removed
+    });
+    t.members.push(cs);
+    return t;
+  }
 
   // TODO - async?
   // if (def.optional) {
   //   t.flags = dom.DeclarationFlags.Optional;
   // }
-  return dom.create.function(def.name, par, ret);
+  const fnType = dom.create.functionType(par, ret);
+  return dom.create.alias(def.name, fnType);
 };
