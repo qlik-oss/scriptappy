201b12206ee5cbf7471c7fc5fa9477042f5a6a17
Thomas <thomas.bengtsson@qlik.com>
2022-01-31 13:44:41 +0100

    fix(to-dts): move isEntry

--- packages/to-dts/lib/types/function.js
+++ packages/to-dts/lib/types/function.js
@@ -5,14 +5,14 @@ const params = require('./params');
 module.exports = function fn(def, tsParent, g, isEntry) {
   const par = params(def.params || [], def.this, g);
   const ret = def.returns ? g.getType(def.returns) : dom.type.void;
-  if (isEntry) {
-    return dom.create.function(def.name, par, ret);
-  }
   if (tsParent && ['class', 'interface', 'object'].includes(tsParent.kind)) {
     return dom.create.method(def.name, par, ret);
   }
   if (tsParent && ['union', 'array', 'parameter', 'alias'].includes(tsParent.kind)) {
     return dom.create.functionType(par, ret);
+  }
+  if (isEntry) {
+    return dom.create.function(def.name, par, ret);
   }
 
   // TODO - async?
