96bda98f2e93bda5622d63f9550493583737b8eb
Johan Enell <johan.enell@qlik.com>
2026-03-21 09:08:10 +0100

    fix(to-dts): support dts-dom 3.7 interface typeParameters
    Signed-off-by: Johan Enell <johan.enell@qlik.com>

--- packages/to-dts/lib/types/interface.js
+++ packages/to-dts/lib/types/interface.js
@@ -5,18 +5,10 @@ const typeParams = require('./type-params');
 /* eslint no-param-reassign: 0 */
 
 module.exports = function fn(def, tsParent, g) {
-  let tt = '';
+  const t = dom.create.interface(def.name);
   if (def.templates) {
-    // dts-dom has no support for typeParameters on interface yet,
-    // so we create a class and extract the typeParameters from it
-    const cc = dom.create.class('XXX');
-    cc.typeParameters = typeParams(def.templates, false, g);
-    const mt = dom.emit(cc).match(/class XXX(.*) {\r/);
-    if (mt && mt[1]) {
-      [, tt] = mt;
-    }
+    t.typeParameters = typeParams(def.templates, false, g);
   }
-  const t = dom.create.interface(`${def.name}${tt}`);
   if (def.returns || def.params) {
     const rt = def.returns ? g.getType(def.returns) : dom.type.void;
     const cs = dom.create.callSignature(params(def.params, def.this, g), rt);
